b2871fbb2c71a2eb7c2d59beac89cbf899e0a46f
Julia Whelan <julia@juliawhelan.com>
2016-03-31 21:31:13 -0700

    Initial Commit

---
+++ FirstTry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstTry
+//
+//  Created by Julia Whelan on 3/31/16.
+//  Copyright © 2016 Julia Whelan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
