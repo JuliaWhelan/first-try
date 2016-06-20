d5152c15b8f648a96943e2dbf1d52ebf610768e9
Julia Whelan <julia@juliawhelan.com>
2016-06-20 11:09:51 -0700

    Added files

--- FirstTry/ViewController.swift
+++ FirstTry/ViewController.swift
@@ -7,16 +7,26 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // set initial location in New Orleans
+        let initialLocation = CLLocation(latitude: 29.950849, longitude: -90.071754)
+        let regionRadius: CLLocationDistance = 2000
+        func centerMapOnLocation(location: CLLocation) {
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                      regionRadius * 2.0, regionRadius * 2.0)
+            mapView.setRegion(coordinateRegion, animated: true)
+        }
+        centerMapOnLocation(initialLocation)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
---
+++ LTNolaArtwork.swift
@@ -0,0 +1,30 @@
+//
+//  LTNolaArtwork.swift
+//  FirstTry
+//
+//  Created by Julia Whelan on 4/9/16.
+//  Copyright © 2016 Julia Whelan. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class LTNolaArtwork: NSObject, MKAnnotation {
+    let title: String
+    let locationName: String
+    let discipline: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.discipline = discipline
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String {
+        return locationName
+    }
+}
